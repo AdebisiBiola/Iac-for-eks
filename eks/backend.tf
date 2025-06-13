0d31530e7a42157a1dcdb9caefae467479712a60
AdebisiBiola <139634031+AdebisiBiola@users.noreply.github.com>
2025-06-14 00:37:24 +0100

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-2amcloud-tf-bucket"
+    bucket         = "dev-2amcloud-tf-buckett"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
