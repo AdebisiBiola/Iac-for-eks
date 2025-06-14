4866baf173fddc6787ba38602c853ae7248e7732
AdebisiBiola <139634031+AdebisiBiola@users.noreply.github.com>
2025-06-14 17:19:10 +0100

    Update iam.tf

--- module/iam.tf
+++ module/iam.tf
@@ -69,7 +69,7 @@ resource "aws_iam_role_policy_attachment" "eks-AmazonEBSCSIDriverPolicy" {
 }
 
 # OIDC
-resource "aws_iam_role" "eks_oidc" {
+resource "aws_iam_role" "dev_eks_oidc" {
   assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role_policy.json
   name               = "eks-oidc"
 }
